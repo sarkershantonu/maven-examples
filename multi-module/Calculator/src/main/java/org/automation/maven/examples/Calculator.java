1b270908248ea54f86b7080327c7a4f0386dd919
shantonu <sarker.shantonu@gmail.com>
2022-10-26 00:11:57 -0500

    Multi module maven project example: app class

---
+++ multi-module/Calculator/src/main/java/org/automation/maven/examples/Calculator.java
@@ -0,0 +1,12 @@
+package org.automation.maven.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator
+{
+    public double add(double a, double b){return a+b;}
+    public int add(int a, int b){return a+b;}
+    public long add(long a, long b){return a+b;}
+}
---
+++ multi-module/calculator-tests/src/main/java/org/automation/maven/examples/AdhocCalculatorTests.java
@@ -0,0 +1,11 @@
+package org.automation.maven.examples;
+
+import org.automation.maven.examples.core.CalculatorTestBase;
+
+/**
+ * Hello world!
+ *
+ */
+public class AdhocCalculatorTests extends CalculatorTestBase {
+
+}
