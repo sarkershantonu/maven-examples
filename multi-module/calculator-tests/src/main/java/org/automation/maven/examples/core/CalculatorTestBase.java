c1b52e87bad338cb37ed40ba82c85150b88d0bd2
shantonu <sarker.shantonu@gmail.com>
2022-10-26 07:06:22 -0500

    test added

--- multi-module/calculator-tests/src/main/java/org/automation/maven/examples/core/CalculatorTestBase.java
+++ multi-module/calculator-tests/src/main/java/org/automation/maven/examples/core/CalculatorTestBase.java
@@ -1,4 +1,15 @@
 package org.automation.maven.examples.core;
 
+import org.automation.maven.examples.Calculator;
+import org.automation.maven.examples.reporting.TestLoggerExtension;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(TestLoggerExtension.class)
 public abstract class CalculatorTestBase {
+    protected static Calculator calculator;
+    @BeforeAll
+    public static void initClass(){
+        calculator = new Calculator();
+    }
 }
